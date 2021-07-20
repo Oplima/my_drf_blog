f527e43c278bf1b3313ec8e1866c0af2079783a0
oplima <oplima@yandex.ru>
2021-07-20 17:36:13 +0300

    Add feedback

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, TagDetailView, TagView, AsideView#, FeedBackView, RegisterView, ProfileView, AddCommentView, GetCommentsView
+from .views import PostViewSet, TagDetailView, TagView, AsideView, FeedBackView#, RegisterView, ProfileView, AddCommentView, GetCommentsView
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
@@ -10,7 +10,7 @@ urlpatterns = [
     path("tags/", TagView.as_view()),
     path("tags/<slug:tag_slug>/", TagDetailView.as_view()),
     path("aside/", AsideView.as_view()),
-#    path("feedback/", FeedBackView.as_view()),
+    path("feedback/", FeedBackView.as_view()),
 #    path('register/', RegisterView.as_view()),
 #    path('profile/', ProfileView.as_view()),
 #    path("comments/", AddCommentView.as_view()),
