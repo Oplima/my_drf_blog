c1e9d74a34d26e35e8e73c05b01e44eb8fb03856
oplima <oplima@yandex.ru>
2021-07-20 16:40:03 +0300

    Add Tags on project

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet#, TagDetailView, TagView, AsideView, FeedBackView, RegisterView, ProfileView, AddCommentView, GetCommentsView
+from .views import PostViewSet, TagDetailView, TagView#, AsideView, FeedBackView, RegisterView, ProfileView, AddCommentView, GetCommentsView
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 
 urlpatterns = [
     path("", include(router.urls)),
-#    path("tags/", TagView.as_view()),
-#    path("tags/<slug:tag_slug>/", TagDetailView.as_view()),
+    path("tags/", TagView.as_view()),
+    path("tags/<slug:tag_slug>/", TagDetailView.as_view()),
 #    path("aside/", AsideView.as_view()),
 #    path("feedback/", FeedBackView.as_view()),
 #    path('register/', RegisterView.as_view()),
