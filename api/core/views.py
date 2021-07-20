7743a0081e5b108b271db154776a554623430832
oplima <oplima@yandex.ru>
2021-07-20 16:19:42 +0300

    DRF project

---
+++ api/core/views.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets, permissions, pagination
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework.response import Response
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
